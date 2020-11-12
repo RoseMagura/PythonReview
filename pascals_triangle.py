d7faed2e281ab43f7950f00a35c19a0c756928c2
RoseMagura <magurarm@guilford.edu>
2020-11-12 12:44:48 -0500

    Feat: My Pascal solution

---
+++ pascals_triangle.py
@@ -0,0 +1,55 @@
+# Problem Statement
+
+#Find and return the nth row of Pascal's triangle in the form a list. 
+# n is 0-based.
+
+def nth_row_pascal(n):
+    """
+    :param: - n - index (0 based)
+    return - list() representing nth row of Pascal's triangle
+    """
+    expo = 11 ** n
+    list = []
+    for i in range(len(str(expo))):
+        digit = str(expo)[i]
+        list.append(int(digit))
+    return list
+
+def test_function(test_case):
+    n = test_case[0]
+    solution = test_case[1]
+    output = nth_row_pascal(n)
+    if solution == output:
+        print("Pass")
+    else:
+        print("Fail")
+
+n = 0
+solution = [1]
+
+test_case = [n, solution]
+test_function(test_case)
+
+n = 1
+solution = [1, 1]
+
+test_case = [n, solution]
+test_function(test_case)
+
+n = 2
+solution = [1, 2, 1]
+
+test_case = [n, solution]
+test_function(test_case)
+
+n = 3
+solution = [1, 3, 3, 1]
+
+test_case = [n, solution]
+test_function(test_case)
+
+n = 4
+solution = [1, 4, 6, 4, 1]
+
+test_case = [n, solution]
+test_function(test_case)
