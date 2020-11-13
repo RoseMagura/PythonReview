47452db191833e16885260e457c7950c6c755c57
RoseMagura <magurarm@guilford.edu>
2020-11-13 14:30:17 -0500

    Edit: My solution passes all tests

---
+++ swap_nodes.py
@@ -0,0 +1,130 @@
+"""
+Given a linked list, swap the two nodes present at position i and j, 
+assuming 0 <= i <= j. The positions are based on 0-based indexing.
+"""
+
+class Node:
+    """LinkedListNode class to be used for this problem"""
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+"""
+:param: head- head of input linked list
+:param: `position_one` - indicates position (index) ONE
+:param: `position_two` - indicates position (index) TWO
+return: head of updated linked list with nodes swapped
+
+TODO: complete this function and swap nodes present at position_one and position_two
+Do not create a new linked list
+"""
+def swap_nodes(head, left_index, right_index):
+    if (0 <= left_index <= right_index) is False:
+        return None
+
+    current = head
+    counter = 0
+    while current:
+        next_node = current.next
+        if left_index == 0:
+            one_current = head
+        if counter == left_index - 1:
+            one_previous = current
+            one_current = current.next
+        if counter == right_index - 1:
+            two_previous = current
+            two_current = current.next
+
+            if left_index != 0:
+                one_previous.next = two_current
+            else:
+                head = two_current
+            two_previous.next = one_current
+            next1 = one_current.next
+            next2 = two_current.next
+            one_current.next = next2
+            two_current.next = next1
+        counter += 1
+        current = next_node
+    return head
+
+def test_function(test_case):
+    head = test_case[0]
+    left_index = test_case[1]
+    right_index = test_case[2]
+    
+    left_node = None
+    right_node = None
+    
+    temp = head
+    index = 0
+    try:
+        while temp is not None:
+            if index == left_index:
+                left_node = temp
+            if index == right_index:
+                right_node = temp
+                break
+            index += 1
+            temp = temp.next
+
+        updated_head = swap_nodes(head, left_index, right_index)
+
+        temp = updated_head
+        index = 0
+        pass_status = [False, False]
+
+        while temp is not None:
+            if index == left_index:
+                pass_status[0] = temp is right_node
+            if index == right_index:
+                pass_status[1] = temp is left_node
+
+            index += 1
+            temp = temp.next
+
+        if pass_status[0] and pass_status[1]:
+            print("Pass")
+        else:
+            print("Fail")
+        return updated_head
+    except Exception as e:
+        print("Fail")
+
+# helper functions for testing purpose
+def create_linked_list(arr):
+    if len(arr)==0:
+        return None
+    head = Node(arr[0])
+    tail = head
+    for data in arr[1:]:
+        tail.next = Node(data)
+        tail = tail.next
+    return head
+
+def print_linked_list(head):
+    while head:
+        print(head.data, end=" ")
+        head = head.next
+    print()
+
+arr = [3, 4, 5, 2, 6, 1, 9]
+head = create_linked_list(arr)
+left_index = 3
+right_index = 4
+test_case = [head, left_index, right_index]
+updated_head = test_function(test_case)
+
+arr = [3, 4, 5, 2, 6, 1, 9]
+left_index = 2 
+right_index = 4
+head = create_linked_list(arr)
+test_case = [head, left_index, right_index]
+updated_head = test_function(test_case)
+
+arr = [3, 4, 5, 2, 6, 1, 9]
+left_index = 0
+right_index = 1
+head = create_linked_list(arr)
+test_case = [head, left_index, right_index]
+updated_head = test_function(test_case)
